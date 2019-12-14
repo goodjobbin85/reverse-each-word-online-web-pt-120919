ef84dae21008360be5d98618cf86c22cca0fd982
thomas dubiel <td1729@hotmail.com>
2019-12-14 19:06:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,8 @@ def reverse_each_word(sentence)
   new_sentence = sentence.split 
   new_sentence.map do |word| 
     word.reverse 
-  end
+  end 
+  new_sentence
 end
 
 reverse_each_word("Hello there")
