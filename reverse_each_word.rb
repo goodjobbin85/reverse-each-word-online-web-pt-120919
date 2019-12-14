53dfb1fe82b95aec9c613b54466e173a1723c0b0
thomas dubiel <td1729@hotmail.com>
2019-12-14 19:05:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(sentence) 
-  sentence.split
-  new_sentence = sentence.join(", ")
-  puts sentence
+  new_sentence = sentence.split
+  puts new_sentence
 end
 
 reverse_each_word("Hello there")
